81cd432f1f7e93c81f806db796b194981c865f19
DKhorobrykh <horobryh.daniil@yandex.ru>
2025-04-09 04:23:27 +0500

    fix JAAS

--- src/main/java/ru/itmo/blps/labs/filter/JwtAuthFilter.java
+++ src/main/java/ru/itmo/blps/labs/filter/JwtAuthFilter.java
@@ -6,41 +6,15 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
-import javax.security.auth.Subject;
-import javax.security.auth.callback.CallbackHandler;
-import javax.security.auth.callback.NameCallback;
-import javax.security.auth.callback.UnsupportedCallbackException;
-import javax.security.auth.login.AppConfigurationEntry;
-import javax.security.auth.login.Configuration;
-import javax.security.auth.login.LoginContext;
-import javax.security.auth.login.LoginException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.authentication.jaas.JaasAuthenticationProvider;
 import org.springframework.security.authentication.jaas.JaasAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
-import ru.itmo.blps.labs.config.SimpleJaasConfiguration;
-import ru.itmo.blps.labs.domain.CustomUserDetails;
-import ru.itmo.blps.labs.domain.Role;
-import ru.itmo.blps.labs.security.CustomJaasLoginModule;
-import ru.itmo.blps.labs.security.RolePrincipal;
-import ru.itmo.blps.labs.security.UserPrincipal;
 import ru.itmo.blps.labs.service.JwtProvider;
-import ru.itmo.blps.labs.service.JwtUtils;
 import ru.itmo.blps.labs.service.UserService;
 
 @Slf4j
@@ -66,64 +40,16 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             Long userId = Long.valueOf(claims.get("userId").toString());
 
             try {
-                CallbackHandler callbackHandler = callbacks -> {
-                    for (var callback : callbacks) {
-                        if (callback instanceof NameCallback nameCallback) {
-                            nameCallback.setName(userId.toString());
-                        } else {
-                            throw new UnsupportedCallbackException(callback, "Unknown callback");
-                        }
-                    }
-                };
-
-                Configuration config = new Configuration() {
-                    @Override
-                    public AppConfigurationEntry[] getAppConfigurationEntry(String name) {
-                        Map<String, Object> options = new HashMap<>();
-                        options.put("userService", userService);
-
-                        return new AppConfigurationEntry[]{
-                            new AppConfigurationEntry(
-                                CustomJaasLoginModule.class.getName(),
-                                AppConfigurationEntry.LoginModuleControlFlag.REQUIRED,
-                                options
-                            )
-                        };
-                    }
-                };
-
-                var loginContext = new LoginContext("JAAS_AUTH", null, callbackHandler, config);
-                loginContext.login();
-
-                var authorities = userService.getById(userId)
-                    .getRoles()
-                    .stream()
-                    .flatMap(role -> {
-                        Set<GrantedAuthority> roleAuthorities = new HashSet<>();
-                        roleAuthorities.add(new SimpleGrantedAuthority("ROLE_" + role.getName()));
-
-                        Set<GrantedAuthority> permissionAuthorities = role.getPrivileges()
-                            .stream()
-                            .map(permission -> new SimpleGrantedAuthority(permission.getName()))
-                            .collect(Collectors.toSet());
-
-                        roleAuthorities.addAll(permissionAuthorities);
-
-                        return roleAuthorities.stream();
-                    })
-                    .toList();
 
                 var authToken = new JaasAuthenticationToken(
                     userId,
-                    null,
-                    authorities,
-                    loginContext
+                    null, null
                 );
 
                 var authenticated = jaasAuthenticationProvider.authenticate(authToken);
                 SecurityContextHolder.getContext().setAuthentication(authenticated);
 
-            } catch (LoginException ex) {
+            } catch (Exception ex) {
                 log.error("Login failed", ex);
                 throw new RuntimeException("Login failed", ex);
             }
--- src/main/java/ru/itmo/blps/labs/security/CustomJaasLoginModule.java
+++ src/main/java/ru/itmo/blps/labs/security/CustomJaasLoginModule.java
@@ -1,38 +1,24 @@
 package ru.itmo.blps.labs.security;
 
-import java.io.IOException;
 import java.security.Principal;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import javax.security.auth.Subject;
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.NameCallback;
-import javax.security.auth.callback.PasswordCallback;
-import javax.security.auth.callback.TextInputCallback;
-import javax.security.auth.callback.UnsupportedCallbackException;
-import javax.security.auth.login.LoginException;
 import javax.security.auth.spi.LoginModule;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
-import ru.itmo.blps.labs.domain.Role;
-import ru.itmo.blps.labs.domain.User;
 import ru.itmo.blps.labs.exception.CustomException;
 import ru.itmo.blps.labs.exception.ExceptionEnum;
-import ru.itmo.blps.labs.service.AuthService;
 import ru.itmo.blps.labs.service.UserService;
 
 @Slf4j
 @RequiredArgsConstructor
 public class CustomJaasLoginModule implements LoginModule {
+
     private Subject subject;
     private CallbackHandler callbackHandler;
     private UserService userService;
@@ -40,7 +26,12 @@ public class CustomJaasLoginModule implements LoginModule {
     private Set<Principal> principals = new HashSet<>();
 
     @Override
-    public void initialize(Subject subject, CallbackHandler callbackHandler, Map<String, ?> sharedState, Map<String, ?> options) {
+    public void initialize(
+        Subject subject,
+        CallbackHandler callbackHandler,
+        Map<String, ?> sharedState,
+        Map<String, ?> options
+    ) {
         this.subject = subject;
         this.callbackHandler = callbackHandler;
         this.userService = (UserService) options.get("userService");
